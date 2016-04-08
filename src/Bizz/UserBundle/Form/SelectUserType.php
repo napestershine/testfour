644a8a31cfea82a44b305c96c1f04edfeda882cc
Manoj <napestershine@gmail.com>
2016-04-08 14:19:43 +0530

    added user type

--- src/Bizz/UserBundle/Form/SelectUserType.php
+++ src/Bizz/UserBundle/Form/SelectUserType.php
@@ -1,17 +1,43 @@
 <?php
+namespace Bizz\UserBundle\Form;
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
-class CreateVehicleStep2Form extends AbstractType {
+class SelectUserType extends AbstractType
+{
 
-public function buildForm(FormBuilderInterface $builder, array $options) {
-$builder->add('engine', 'MyCompany\MyBundle\Form\Type\VehicleEngineType', array(
-'placeholder' => '',
-));
-}
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('usertype', ChoiceType::class, array(
+                'choices' => array(
+                    'i' => 'Individual',
+                    'c' => 'Company'
+                ),
+                'required' => true
+            ))
+            ->add('category', EntityType::class, array(
+                'class' => 'Bizz\AdminBundle:CompCat',
+                'label' => 'Select Category',
+                'placeholder' => '-- Select Category --',
+                'choice_label' => 'category',
+                'required' => true
+            ))
+            ->add('category', EntityType::class, array(
+                'class' => 'Bizz\AdminBundle:Icat',
+                'label' => 'Select Category',
+                'placeholder' => '-- Select Category --',
+                'choice_label' => 'category',
+                'required' => true
+            ));
+    }
 
-public function getBlockPrefix() {
-return 'createVehicleStep2';
-}
+    public function getBlockPrefix()
+    {
+        return 'SelectUserType';
+    }
 
 }
