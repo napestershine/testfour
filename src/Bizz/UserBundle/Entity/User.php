b683830d61d60550d0ee3c30ae2ca75dac32a829
Manoj <napestershine@gmail.com>
2016-04-08 14:42:44 +0530

    integrating HWIOAuthBundle

--- src/Bizz/UserBundle/Entity/User.php
+++ src/Bizz/UserBundle/Entity/User.php
@@ -5,6 +5,7 @@ namespace Bizz\UserBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity
